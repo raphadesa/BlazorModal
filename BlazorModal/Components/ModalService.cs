13df90f0cccc91e77219eec36de9baade9dba19c
desalbres <raphael.desalbres@outlook.com>
2021-01-06 13:55:34 +0100

    Ajoutez des fichiers projet.

---
+++ BlazorModal/Components/ModalService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorModal
+{
+    public class ModalService
+    {
+        public event Action OnShow;
+        public event Action OnClose;
+
+        public void Show()
+        {   
+            OnShow?.Invoke();
+        }
+
+        public void Hide()
+        {
+            OnClose?.Invoke();
+        }
+    }
+}
